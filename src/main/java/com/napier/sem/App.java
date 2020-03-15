487f3955609cdaa15bfa6310dd247dbda6f581d5
Andreas Steyven <a.steyven@napier.ac.uk>
2020-03-15 00:09:19 +0000

    Added employee output

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -105,7 +105,20 @@ public class App
         }
     }
 
-
+    public void displayEmployee(Employee emp)
+    {
+        if (emp != null)
+        {
+            System.out.println(
+                    emp.emp_no + " "
+                            + emp.first_name + " "
+                            + emp.last_name + "\n"
+                            + emp.title + "\n"
+                            + "Salary:" + emp.salary + "\n"
+                            + emp.dept_name + "\n"
+                            + "Manager: " + emp.manager + "\n");
+        }
+    }
 
     public static void main(String[] args)
     {
@@ -114,6 +127,11 @@ public class App
 
         // Connect to database
         a.connect();
+
+        // Get Employee
+        Employee emp = a.getEmployee(255530);
+        // Display results
+        a.displayEmployee(emp);
 
         // Disconnect from database
         a.disconnect();
